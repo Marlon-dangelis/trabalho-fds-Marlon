773b4509f076b4bb6b85d013dda5a1c4aed05ca8
Marlon-dangelis <marlon.dangelis.r@gmail.com>
2025-04-28 23:01:54 -0300

    Add files via upload

---
+++ button.js
@@ -0,0 +1,8 @@
+let button = document.querySelector("#button");
+let mensagem = document.querySelector("#mensagem");
+
+button.addEventListener("click", resposta);
+
+function resposta(){
+mensagem.innerHTML ="Você não precisa dar passos gigantes todos os dias. O importante é continuar se movendo. Até o menor progresso, quando somado ao tempo e à persistência, constrói grandes vitórias. Confie no processo e siga em frente"
+}
